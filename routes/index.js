d2862fd7cfebb93d78eb08fd9d3f08f917f77a0c
Chadley Maggott <chadleym@bbd.co.za>
2022-05-15 19:37:04 +0200

    fixed and adding missing images

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/how-to-solve', function(req, res, next) {
-  res.render('how-to-solve', { title: 'Express' });
+router.get('/how-to-solve.html', function(req, res, next) {
+  res.render('how-to-solve', { title: 'How to Solve' });
 });
 
 module.exports = router;
