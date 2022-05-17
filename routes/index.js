17e1f267bbf592fc1b4302d1f82d6a889fae295d
Chadley Maggott <chadleym@bbd.co.za>
2022-05-17 17:32:47 +0200

    integrated nav bar and global styling sheet

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/how-to-solve.html', function(req, res, next) {
-  res.render('how-to-solve', { title: 'How to Solve' });
+//For any other get request to render need the full path or request
+router.get('/about/about.html', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/play-game/play-game.html', function(req, res, next) {
+  res.render('play-game', { title: 'Play Game' });
+});
+
+router.get('/how-to-play/how-to-play.html', function(req, res, next) {
+  res.render('how-to-play', { title: 'How to Play' });
+});
+
+router.get('/leaderboard/leaderboard.html', function(req, res, next) {
+  res.render('leaderboard', { title: 'Leaderboard' });
+});
+
+router.get('algorithms/algorithms.html', function(req, res, next) {
+  res.render('algorithms', { title: 'Algorithms' });
 });
 
 module.exports = router;
