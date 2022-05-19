f1bc4337607b2a650280944e221f1a5c0cc42bec
Zaeem Asvat <zaeema@bbd.co.za>
2022-05-19 22:25:38 +0200

    #7: updated play cube UI

---
+++ public/play-game/js/play-game.js
@@ -0,0 +1,6 @@
+
+movesCollapsable = document.getElementById("movesCollapsable");
+movesCollapsable.onclick = function() {
+    dropDownIconMoves = document.getElementById("drop-down-icon-moves");
+    
+};
